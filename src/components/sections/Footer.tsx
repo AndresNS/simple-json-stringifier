99fa8684cf42a031dc143ee59961392a4078b7b1
Andres Navarro S <andresm.navarro@gmail.com>
2023-07-27 14:20:15 -0400

    Change letter spacing in footer links

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -2,7 +2,7 @@ import GithubIcon from "../icons/GithubIcon";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center py-12 gap-4 font-roboto font-bold text-slate-600 dark:text-zinc-200">
+    <footer className="flex flex-col items-center py-12 gap-4 font-roboto font-bold text-slate-600 dark:text-zinc-200 tracking-wide">
       <nav className="flex gap-4">
         <a href="#">Home</a>
         <a href="/about">About</a>
